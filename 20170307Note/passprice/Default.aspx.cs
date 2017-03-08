a2cf3b90920c6853480533baf7a084b39b80b0cd
kancheng <zxdfgcv@gmail.com>
2017-03-09 00:49:24 +0800

    kan edit note

---
+++ 20170307Note/passprice/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        string grade = "C";
+
+        string str = "";
+
+        grade = TextBox1.Text;
+        switch (grade)  // switch條件敘述
+        {
+            case "A":
+                str = "10 NT<br/>";
+                break;
+            case "B":
+                str = "20 NT<br/>";
+                break;
+            case "C":
+                str = "30 NT<br/>";
+                break;
+            case "D":
+                str = "300 NT<br/>";
+                break;
+            case "E":
+                str = "3000 NT<br/>";
+                break;
+
+
+            default:
+                str = "ERROR <br/>";
+                break;
+        }
+        lblOutput.Text += str;
+    }
+}
---
+++ 20170307Note/passpriceTranType/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int choice = int.Parse(TextBox1.Text);
+
+        int price = 00;
+
+        switch (choice)  // switch條件敘述
+        {
+            case 1:
+                price = 100;
+                break;
+            case 2:
+                price = 200;
+                break;
+            case 3:
+                price = 300;
+                break;
+            case 4:
+                price = 400;
+                break;
+            case 5:
+                price = 600;
+                break;
+            default:
+                price = 00;
+                break;
+        }
+        TextBox1.Text = Convert.ToString(price);
+    }
+}
