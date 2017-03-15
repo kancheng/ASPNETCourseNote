417256e7d9f14d363c96e5a767a2a4fa587fcedf
kancheng <zxdfgcv@gmail.com>
2017-03-15 17:36:57 +0800

    upload calArrary HW

---
+++ 20170314_3Note/calArrary/DefaultTest.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DefaultTest.aspx.cs" Inherits="DefaultTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Font-Size="Large" Text="Hi ASP.NET "></asp:Label>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server" Height="59px" Width="187px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="margin-top: 0px" Text="Commit" Width="105px" />
+    
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Max" />
+&nbsp;
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Min" />
+&nbsp;
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Avg" />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170314_3Note/calArrary/DefaultTest.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DefaultTest : System.Web.UI.Page
+{
+
+    string[] Product = { "2.98", "4.50", "9.98", "4.49", "6.87" };
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Array.Sort(Product);
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string temp;
+        temp = Label1.Text;
+        Label1.Text = TextBox1.Text;
+        TextBox1.Text = temp;
+    }
+
+    //Max
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        double temp = 0.0; double max = 0.0;
+
+        foreach (string price in Product)
+        {
+            if (price == null) continue;
+            temp = double.Parse(price);
+            if (max < temp) max = temp;
+        }
+
+        TextBox1.Text = Convert.ToString(max);
+
+    }
+
+    //Min
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        double temp = 0.0; double min = 100000.0;
+
+        foreach (string price in Product)
+        {
+            if (price == null) continue;
+            temp = double.Parse(price);
+            if (min > temp) min = temp;
+        }
+
+        TextBox1.Text = Convert.ToString(min);
+
+    }
+
+    //Avg
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        double temp = 0.0;
+
+        foreach (string price in Product)
+        {
+            if (price == null) continue;
+            temp += double.Parse(price);
+        }
+        temp /= Product.Length;
+
+        TextBox1.Text = Convert.ToString(temp);
+    }
+
+
+}
