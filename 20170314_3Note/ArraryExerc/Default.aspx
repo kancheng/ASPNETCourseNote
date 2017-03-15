721aae65c7185b73ee524464dea4eedfda84cb1c
kancheng <zxdfgcv@gmail.com>
2017-03-15 19:00:09 +0800

    Upload Arrary Exercise

---
+++ 20170314_3Note/ArraryExerc/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <br />
+        <asp:Panel ID="Panel1" runat="server" Height="153px">
+            <asp:Button ID="Button1" runat="server" ForeColor="Blue" OnClick="Button1_Click" style="z-index: 1; left: 15px; top: 68px; position: absolute; height: 20px; right: 1801px;" Text="Button" />
+            <asp:Label ID="Label1" runat="server" style="z-index: 1; left: 19px; top: 101px; position: absolute; height: 27px; width: 138px;" Text="Label"></asp:Label>
+        </asp:Panel>
+        <br />
+        <br />
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170314_3Note/ArraryExerc/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    int i = 0;
+    string[] Product = new string[6];
+
+    double price;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Product[0] = "3.98";
+        Product[1] = "2.98";
+        Product[2] = "4.50";
+        Product[3] = "9.98";
+        Product[4] = "4.49";
+        Product[5] = "6.87";
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        price = 0.0;
+        string result = "";
+        foreach (string choice in Product)
+        {
+            price = double.Parse(choice);
+            if (price >= 1 && price <= 19)
+            {
+                result += price + "<br />";
+            }
+        }
+
+        Label1.Text = result;
+    }
+    
+}
