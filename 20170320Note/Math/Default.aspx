22591db9febe84f3a25072b6998c401cc0750f36
kancheng <zxdfgcv@gmail.com>
2017-03-21 04:17:49 +0800

    Upload ch4 exercise practice 4

---
+++ 20170320Note/Math/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style = "text-align:center">
+        <h1>Math</h1>
+        <br />
+        x值 :
+        <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" OnTextChanged="TextBox1_TextChanged">156</asp:TextBox>
+        <br />
+        <br />
+        y值 :
+        <asp:TextBox ID="TextBox2" runat="server" AutoPostBack="True" OnTextChanged="TextBox2_TextChanged">150</asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170320Note/Math2/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    int a, b;
+    string formula1, formula2, formula3;
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TextBox1.Attributes.Add("style", "font-size:xx-small;color:#666666"); /* 順便改變字的大小顏色 */
+        TextBox1.Attributes.Add("onFocus", "this.value=''"); /* 一點擊TextBox1後，裡面的字馬上被清空 */
+        TextBox2.Attributes.Add("style", "font-size:xx-small;color:#666666");
+        TextBox2.Attributes.Add("onFocus", "this.value=''");
+        a = int.Parse(TextBox1.Text);
+        b = int.Parse(TextBox2.Text);
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        formula1 = Convert.ToString(a * a + 2 * a + 1);
+        Label1.Text = Convert.ToString("計算結果 : <br><br>" + "x * x + 2 * x + 1 = " + formula1);
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e) {}
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e) {}
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        formula2 = Convert.ToString((a + b) * (a + b) + 20);
+        Label1.Text = Convert.ToString("計算結果 : <br><br>" + "(x + y) * (x + y) + 20 = " + formula2);
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        formula3 = Convert.ToString(a * a * a + 3 * b + 5);
+        Label1.Text = Convert.ToString("計算結果 : <br><br>" + "x * x * x + 3 * y + 5 = " + formula3);
+    }
+}
