480dc3ed066a7f8787eb25d0076bf8d7e18e6b67
kancheng <zxdfgcv@gmail.com>
2017-03-20 22:48:47 +0800

    Upload ch3 exercise practice 7

---
+++ 20170314_2Note/bill/Default.aspx.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style = "text-align:center">
+        <h1>Bill</h1>
+        <br />
+        請輸入小時數 :&nbsp; 
+        <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" OnTextChanged="TextBox1_TextChanged"> 50 </asp:TextBox> &nbsp; /hr
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" Width="90px" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170314_2Note/bill/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    double timedata, minute, cost;
+    string opttem;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TextBox1.Attributes.Add("style", "font-size:xx-small;color:#666666"); /* 順便改變字的大小顏色 */
+        TextBox1.Attributes.Add("onFocus", "this.value=''"); /* 一點擊TextBox1後，裡面的字馬上被清空 */
+    }
+    static public double bill(double hour)
+    {
+        double total = 0;
+        if (hour <= 50)
+        {
+            total = (hour * 60) * 0.3;
+        }
+        else if (hour >= 50)
+        {
+                total = (hour * 60) * 0.2;
+        }
+        return total;
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        timedata = double.Parse(TextBox1.Text);
+        cost = bill(timedata);
+        Label1.Text = Convert.ToString("<h2>輸出結果 : </h2><br>" + "費用 : " +  cost );
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+}
