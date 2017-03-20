ccbe29bc722a2fe859aa1e81de712268f1609f07
kancheng <zxdfgcv@gmail.com>
2017-03-21 00:43:56 +0800

    Upload ch3 exercise practice8

---
+++ 20170314_2Note/randomArrary/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style = "text-align:center">
+        <h1>Random Arrary Practice</h1>
+        <br />
+        Range 1 ~ 150.
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" Width="90px" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170314_2Note/randomArrary/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    Random rd = new Random(Guid.NewGuid().GetHashCode());
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int index = rd.Next(1, 151);
+        int[] values = new int[4] ;
+        int temval;
+        for (int tem = 0; tem <= values.Length-1; tem++) {
+            temval = rd.Next(1, 151);
+            values[tem] = temval;
+
+        }
+
+        string temresult = Convert.ToString("第一個亂數 : " + values[0] + "<br>" + "第二個亂數 : " + values[1] + "<br>"+ "第三個亂數 : " + values[2] + "<br>" + "第四個亂數 : " + values[3]);
+        Label1.Text = temresult;
+    }
+
+}
