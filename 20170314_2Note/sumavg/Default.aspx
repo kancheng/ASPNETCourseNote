cf3c70fc5a8a06d4e9d21df6acea976f19d9c916
kancheng <zxdfgcv@gmail.com>
2017-03-20 20:48:00 +0800

    Upload ch3 exercise practice 6

---
+++ 20170314_2Note/sumavg/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style = "text-align:center">
+        <h1>Calculate</h1>
+        <br />
+                Range 71, 62, 58, 86, 92 . <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Max" />&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Min" />&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Sum" />&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Average" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170314_2Note/sumavg/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string[] Product = { "71", "62", "58", "86", "92" };
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Array.Sort(Product);
+    }
+
+    /* max */
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        double temp = 0.0; double max = 0.0;
+
+        foreach (string price in Product)
+        {
+            if (price == null) continue;
+            temp = double.Parse(price);
+            if (max < temp) max = temp;
+        }
+
+        Label1.Text = Convert.ToString("Max : " + max);
+
+    }
+
+
+    /* min */
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        double temp = 0.0; double min = 100000.0;
+
+        foreach (string price in Product)
+        {
+            if (price == null) continue;
+            temp = double.Parse(price);
+            if (min > temp) min = temp;
+        }
+
+        Label1.Text = Convert.ToString("Min : " + min);
+    }
+
+    /* sum */
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        double temp = 0.0;
+
+        foreach (string price in Product)
+        {
+            if (price == null) continue;
+            temp += double.Parse(price);
+        }
+
+        Label1.Text = Convert.ToString("Sum : " + temp);
+    }
+
+    /* average */
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        double temp = 0.0;
+
+        foreach (string price in Product)
+        {
+            if (price == null) continue;
+            temp += double.Parse(price);
+        }
+        temp /= Product.Length;
+
+        Label1.Text = Convert.ToString("Average : " + temp);
+    }
+}
