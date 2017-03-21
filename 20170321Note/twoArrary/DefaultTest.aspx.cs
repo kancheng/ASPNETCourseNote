f10ca53422ed56d06b1a4325953755d3fe8899db
kancheng <zxdfgcv@gmail.com>
2017-03-21 14:13:32 +0800

    Upload Arrary Exercise File

---
+++ 20170321Note/twoArrary/DefaultTest.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DefaultTest : System.Web.UI.Page
+{
+
+
+    string[,] Product  = new string[5, 2];
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TextBox1.Attributes.Add("style", "font-size:xx-small;color:#666666"); /* 順便改變字的大小顏色 */
+        TextBox1.Attributes.Add("onFocus", "this.value=''"); /* 一點擊TextBox1後，裡面的字馬上被清空 */
+        Product[0, 0] = "A"; Product[0, 1] = "2.98";
+        Product[1, 0] = "B"; Product[1, 1] = "3.50";
+        Product[2, 0] = "C"; Product[2, 1] = "4.66";
+        Product[3, 0] = "D"; Product[3, 1] = "1.53";
+        Product[4, 0] = "E"; Product[4, 1] = "9.98";
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        string choice = TextBox1.Text;
+        string price = getPrice(choice);
+        Label1.Text = ("The price is :" + price);
+    }
+    string getPrice(string choice)
+    {
+        string price = "0.0";
+        for (int i = 0; i < 5; i++)
+        {
+            if (choice == Product[i, 0])
+                price = Product[i, 1];
+        }
+        return price;
+    }
+}
