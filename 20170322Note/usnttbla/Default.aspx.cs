9c8416094b66b18c51836ef353981554e8f37f7c
kancheng <zxdfgcv@gmail.com>
2017-03-29 06:46:04 +0800

    Upload ch5 exercise practice 4

---
+++ 20170322Note/usnttbla/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string valus;
+    double temus,temnt;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+        valus = TextBox1.Text;
+        temus = Convert.ToDouble(valus);
+        temnt = temus * 30.0;
+        Label1.Text = Convert.ToString("NT : " + temnt);
+    }
+}
