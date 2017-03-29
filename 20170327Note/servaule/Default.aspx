6927024c0897a793cde3b82aafcefb1a6d4d0c34
kancheng <zxdfgcv@gmail.com>
2017-03-29 22:46:42 +0800

    Upload ch6 book exercise 8

---
+++ 20170327Note/servaule/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+            Server變數:
+            <asp:TextBox ID="txtServer" runat="server" Width="183px"></asp:TextBox>
+            &nbsp;<asp:Button ID="Button1" runat="server" Text="查詢" OnClick="Button1_Click" />
+            　<asp:Button ID="Button2" runat="server" Text="瀏覽器" OnClick="Button2_Click" />
+            <hr />
+            <br />
+            <asp:Label ID="lblOutput" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170327Note/servaule/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        lblOutput.Text = Request.ServerVariables[txtServer.Text];
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        /* 換行標簽常數 */
+        const string BR = "<br />";
+
+        HttpBrowserCapabilities hbc = Request.Browser;
+        lblOutput.Text = " 瀏覽器種類 : " + hbc.Type + BR ;
+        lblOutput.Text += " 瀏覽器名稱 : " + hbc.Browser + BR ;
+        lblOutput.Text += " 版本 : " + hbc.Version + BR ;
+        lblOutput.Text += " 主版本 : " + hbc.MajorVersion + BR ;
+        lblOutput.Text += " 次版本 : " + hbc.MinorVersion + BR ;
+        lblOutput.Text += " 平台 : " + hbc.Platform + BR ;
+        lblOutput.Text += " 支援框架 : " + hbc.Frames + BR ;
+        lblOutput.Text += " 支援表格 : " + hbc.Tables + BR ;
+        lblOutput.Text += " 支援 Cookies : " + hbc.Cookies + BR ;
+        lblOutput.Text += " 支援 VBScript : " + hbc.VBScript + BR;
+        lblOutput.Text += " 支援 JavaScript : " + hbc.JavaScript + BR;
+        lblOutput.Text += " 支援 Java Applets : " + hbc.JavaApplets + BR;
+        lblOutput.Text += " 支援 : ActiveX 控制 " + hbc.ActiveXControls + BR;
+    }
+}
