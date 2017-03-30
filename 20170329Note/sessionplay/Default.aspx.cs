88ede5a840ac81d4abe05ba6841887201f4efd1b
kancheng <zxdfgcv@gmail.com>
2017-03-30 19:04:53 +0800

    Upload ch7 book exercise 6

---
+++ 20170329Note/sessionplay/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        /* 設定 Session 變數 */
+        Session["UserName"] = txtUser.Text;
+        Session["UserPassword"] = txtPass.Text;
+
+        /* 轉址到其他 ASP pages */
+        Response.Redirect("Default2.aspx");
+
+    }
+}
---
+++ 20170329Note/sessionplay/Default2.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string name, password;
+
+        lblOutput.Text = "Session ID : " + Session.SessionID + "<br />";
+
+        if ( Session["UserName"] != null ) {
+
+            /* 取得 Session 變數值 */
+            name = Session["UserName"].ToString();
+            password = Session["UserPassword"].ToString();
+
+            /* 顯示取得 Session 變數值 */
+            lblOutput.Text += "Name : " + name + "<br />";
+            lblOutput.Text += "Password : " + password + "<br />";
+
+            /* 放棄 Session */
+            Session.Abandon();
+
+        }
+
+    }
+}
