966ff119f0d1c8fbbd32149b87436c8b29dfba62
kancheng <zxdfgcv@gmail.com>
2017-03-30 15:23:07 +0800

    Upload ch7 book exercise 3

---
+++ 20170329Note/otherpagebmi/Default2.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack) {
+            double h, w;
+            TextBox txt;
+            txt = (TextBox)PreviousPage.FindControl("txtWeight");
+            w = Convert.ToDouble(txt.Text);
+            txt = (TextBox)PreviousPage.FindControl("txtHeight");
+            h = Convert.ToDouble(txt.Text) / 100.0;
+            lblOutput.Text = "BMI : " + (w / h / h);
+
+        }
+    }
+}
