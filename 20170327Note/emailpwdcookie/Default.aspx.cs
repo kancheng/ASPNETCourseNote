5c70478d2968cc80efb76b3a77cf23e3c5c8e766
kancheng <zxdfgcv@gmail.com>
2017-03-31 03:40:14 +0800

    Upload ch7 exercise practice 4

---
+++ 20170327Note/emailpwdcookie/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if ( TextBox2.ReadOnly == false ) {
+            TextBox2.ReadOnly = true;
+        }
+        if (Button2.Enabled == false) {
+            Button2.Enabled = true;
+        }
+        if (validPassNo.Enabled == true) {
+            validPassNo.Enabled = false;
+        }
+        Response.Cookies["us"]["em"] = Server.UrlEncode(TextBox1.Text);
+        Response.Cookies["us"]["pd"] = Server.UrlEncode(TextBox2.Text);
+        Response.Cookies["us"].Expires = DateTime.Today.AddDays(10);
+
+        if (Panel1.Visible == false) {
+            Panel1.Visible = true;
+            Label1.Text = "Cookies built successfully";
+        }
+
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Default2.aspx");
+    }
+}
---
+++ 20170327Note/emailpwdcookie/Default2.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string em, pd;
+        em = Server.UrlDecode(Request.Cookies["us"]["em"]);
+        pd = Server.UrlDecode(Request.Cookies["us"]["pd"]);
+        if (Request.Cookies["us"] != null) {
+            Label1.Text = "E-Mail : " + em + "<br />";
+            Label1.Text = "Password : " + pd + "<br />";
+        } else {
+            Label1.Text = "Cookies not exist !!";
+        }
+        if (Button1.Enabled == true) {
+            Button1.Enabled = false;
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        DateTime dtDay = DateTime.Today.AddDays(-365);
+        Response.Cookies["us"].Expires = dtDay;
+        Label1.Text = "Remove Cookies !";
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+
+    }
+}
+
