f61b2d75e4ea73fd1105832ef17a02de612e4c06
kancheng <zxdfgcv@gmail.com>
2017-03-31 04:42:11 +0800

    Upload ch7 exercise practice 5

---
+++ 20170329Note/systimesession/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        this.Session["LoginTime"] = DateTime.Now;
+        Label1.Text = "登入時間 : " + this.Session["LoginTime"];
+    }
+
+
+}
+
