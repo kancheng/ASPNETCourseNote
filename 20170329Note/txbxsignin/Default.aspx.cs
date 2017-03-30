2f74c5005e565a3dae8c1d90d76953554929bec2
kancheng <zxdfgcv@gmail.com>
2017-03-30 15:00:10 +0800

    Upload ch7 book exercise 2

---
+++ 20170329Note/txbxsignin/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string name = txtUser.Text;
+        string pass = txtPass.Text;
+        /* 轉址並傳遞 URL 參數至 Default2.aspx */
+        Response.Redirect("Default2.aspx?User=" + Server.UrlEncode(name) + "&Pass= "+pass);
+    }
+}
---
+++ 20170329Note/txbxsignin/Default2.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string Username, Password;
+
+        /* 取得參數值 */
+        Username = Server.UrlDecode(Request.QueryString["User"]);
+
+        Password = Request.QueryString["Pass"];
+
+        /* 顯示取得的參數值 */
+
+        lblOutput.Text = " 名稱 : " + Username + "<br />";
+        lblOutput.Text += " 密碼 : " + Password + "<br />";
+
+    }
+}
