0ebd6c8a2632ccd07003ea4862c148dffdeba1ce
kancheng <zxdfgcv@gmail.com>
2017-04-01 03:49:32 +0800

    Upload ch8 book exercise 3

---
+++ 20170330Note/seltabpra/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+            SQL 指令:
+            <asp:TextBox ID="txtSQL" runat="server" Width="230px">SELECT * FROM Students</asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="查詢單一欄位值" OnClick="Button1_Click" />
+            &nbsp;<br />
+            <asp:Label ID="lblOutput" runat="server" ForeColor="Blue"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170330Note/seltabpra/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlConnection objCon;
+        string strDbCon;
+
+        /* DB Connect string */
+        strDbCon = "Data Source = (LocalDB)\\MSSQLLocalDB;" +
+            "AttachDbFilename=" +
+            Server.MapPath("App_Data\\School.mdf") + "; Integrated Security = True";
+
+        /* build Connection Object*/
+        objCon = new SqlConnection(strDbCon);
+
+        /* Start DB Connect */
+        objCon.Open();
+
+        /* build Command Object by SQL Command  Line*/
+        SqlCommand objCmd = new SqlCommand(txtSQL.Text, objCon);
+
+        /* use ExecuteScalar run SQL Command */
+        lblOutput.Text = "SELECT : " + objCmd.ExecuteScalar();
+
+        /* Close DB Connect */
+        objCon.Close();
+
+    }
+}
