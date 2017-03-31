a84a160b90524bf105f534f182c01d0d908d4581
kancheng <zxdfgcv@gmail.com>
2017-04-01 05:46:29 +0800

    Upload ch8 exercise practice 1

---
+++ 20170330Note/coursedbtbpra/App_Data/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[AddressBooks]
+(
+	[No] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL, 
+    [Address] NCHAR(10) NULL, 
+    [Telephone] NCHAR(10) NULL, 
+    [Birthday] NCHAR(10) NULL, 
+    [Email] NCHAR(10) NULL
+)
