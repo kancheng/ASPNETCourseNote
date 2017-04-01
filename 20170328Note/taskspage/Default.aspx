dfa1b5772b96522db7658089f96cda1cb99e25d7
kancheng <zxdfgcv@gmail.com>
2017-04-01 20:22:32 +0800

    Upload course exercise

---
+++ 20170328Note/taskspage/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style = "text-align:center">
+            <h1>Tasks</h1>
+        <br />
+        <div>
+            <asp:RadioButton ID="RadioButton1" runat="server" OnCheckedChanged="RadioButton1_CheckedChanged" GroupName="Product" Text="Insert" />
+            &nbsp;
+            <asp:RadioButton ID="RadioButton2" runat="server" OnCheckedChanged="RadioButton2_CheckedChanged" GroupName="Product" Text="Delete" />
+            &nbsp;
+            <asp:RadioButton ID="RadioButton3" runat="server" OnCheckedChanged="RadioButton3_CheckedChanged" GroupName="Product" Text="Modify" />
+            &nbsp;
+            <asp:RadioButton ID="RadioButton4" runat="server" OnCheckedChanged="RadioButton4_CheckedChanged" GroupName="Product" Text="Query" />
+        </div>
+        <div>
+
+            Number&nbsp;:&nbsp;<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            Data&nbsp;:&nbsp;<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+
+        </div>
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        </div>
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170328Note/taskspage/Default.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+   static double[] Product = new double[20];
+
+    static _Default()
+    {
+        Product[0] = 2.98;
+        Product[1] = 4.50;
+        Product[2] = 9.98;
+        Product[3] = 4.49;
+        Product[4] = 6.87;
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        /*
+        if (RadioButton4.Checked){
+            TextBox2.ReadOnly = true;
+        }*/
+
+    }
+
+    protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void RadioButton3_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void RadioButton4_CheckedChanged(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        double tmnumber, tmprice;
+        string strtnb, strtprc;
+        
+        strtnb = TextBox1.Text;
+        strtprc = TextBox2.Text;
+        Label1.Text = Convert.ToString(TextBox2.Text);
+
+        tmnumber = 10;
+        tmprice = 5.5;
+        if (!strtprc.Equals(""))
+        {
+            tmprice = Double.Parse(strtprc);
+        }
+        else
+        {
+            tmprice = 0;
+        }
+        tmnumber = Double.Parse(strtnb);
+
+        /* modify */
+        if (RadioButton3.Checked) {
+            Label1.Text = Convert.ToString("Modify Data : " + modifyPrice(tmnumber, tmprice));
+        }
+        /* insert */
+        if (RadioButton1.Checked)
+        {
+            Label1.Text = Convert.ToString("Insert Data : " + insertPrice(tmnumber, tmprice));
+        }
+
+        /* query */
+        if (RadioButton4.Checked)
+        { 
+            Label1.Text = Convert.ToString("Query Data : " + queryPrice(tmnumber));
+        }
+        /* delete */
+        if (RadioButton2.Checked)
+        {
+            Label1.Text = Convert.ToString("Delete Data : " + deletePrice(tmnumber));
+        }
+    }
+
+    double deletePrice(double number)
+    {
+        int mnub = Convert.ToInt32(number);
+        double idex = 0;
+        if (Product[mnub] > 0)
+        {
+            Product[mnub] = 0;
+            idex = Product[mnub];
+        }
+        return idex;
+
+    }
+    double queryPrice(double number)
+    {
+        int mnub = Convert.ToInt32(number);
+        double idex = 0;
+        if (Product[mnub] > 0)
+        {
+            idex = Product[mnub];
+
+        }
+        return idex;
+    }
+    double modifyPrice(double number, double txtdata)
+    {
+        int mnub = Convert.ToInt32(number);
+        double idex = txtdata;
+        if (Product[mnub] > 0){
+                Product[mnub] = txtdata;
+                idex = Product[mnub];
+        }
+        return idex;
+    }
+
+    double insertPrice(double number, double txtdata)
+    {
+            int mnub = Convert.ToInt32(number);
+            if (Product[mnub] == 0)
+            {
+                Product[mnub] = txtdata;
+            }
+            return txtdata;
+    }
+}
