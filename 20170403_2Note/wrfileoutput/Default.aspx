2590d70c1dce6882d5752a8123e016f4fc8a4f9a
kancheng <zxdfgcv@gmail.com>
2017-04-06 05:00:35 +0800

    Upload ch14 book exercise 3

---
+++ 20170403_2Note/wrfileoutput/Default.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+
+        .style1
+        {
+            width: 100%;
+        }
+        .style2
+        {
+            width: 139px;
+        }
+        .style3
+        {
+            color: #0000FF;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="style1">
+                <tr>
+                    <td class="style2"><span class="style3">讀幾字元或幾行</span>：</td>
+                    <td>
+                        <asp:TextBox ID="txtCount" runat="server">2</asp:TextBox>
+                    </td>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Text="讀取字元" Width="80px" OnClick="Button1_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style2">
+                        <asp:Button ID="Button2" runat="server" Text="讀取幾行" Width="80px" OnClick="Button2_Click" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button3" runat="server" Text="讀取至檔尾" Width="80px" OnClick="Button3_Click" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button4" runat="server" Text="讀取整個檔案" Width="80px" OnClick="Button4_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <asp:TextBox ID="txtOutput" runat="server" Height="107px" ReadOnly="True" TextMode="MultiLine" Width="397px"></asp:TextBox>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 20170403_2Note/wrfileoutput/Default.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int ch, i, count;
+
+        /* 取得實際路徑 */
+        string path = Server.MapPath( "Output.txt" );
+        count = Convert.ToInt32( txtCount.Text );
+
+        /* 開啟文字檔案 */
+        StreamReader sr = new StreamReader(path);
+
+        for ( i = 1; i <= count; i++ ) {
+
+            /* 讀取字元 */
+            ch = sr.Read();
+            txtOutput.Text += (char)(ch) + " ";
+        }
+        txtOutput.Text += "\r\n";
+
+        /* Close File */
+        sr.Close();
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int i, count;
+        string path, str;
+
+        /* 取得實際路徑 */
+        path = Server.MapPath("Output.txt");
+        count = Convert.ToInt32(txtCount.Text);
+
+        /* 開始文字檔案 */
+        StreamReader sr = new StreamReader(path);
+
+        for ( i = 1; i <= count; i++) {
+            /* 讀取一行 */
+            str = sr.ReadLine();
+            txtOutput.Text += str + "\r\n";
+        }
+
+        /* 關閉檔案 */
+        sr.Close();
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        /* 取得實際路徑 */
+        string path = Server.MapPath("Output.txt");
+
+        /* 開啟文字檔案 */
+        StreamReader sr = new StreamReader(path);
+
+        /* 讀至檔尾 */
+        string str = sr.ReadToEnd();
+
+        txtOutput.Text += str + "\r\n";
+
+        /* 關閉檔案 */
+        sr.Close();
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        int count = 0;
+        string textLine, path;
+
+        /* 取得實際路徑 */
+        path = Server.MapPath("Output.txt");
+
+        /* 開啟檔案 */
+        StreamReader sr = new StreamReader(path);
+
+        do {
+            textLine = sr.ReadLine();
+            count += 1;
+            txtOutput.Text += count + " : " + textLine + "\r\n" ;
+        } while ( !( sr.Peek() == -1) );
+
+        /* Close File */
+        sr.Close();
+    }
+}
