a2ea9b5af608ff619953194e6fc462fff26b6a82
kancheng <zxdfgcv@gmail.com>
2017-04-06 04:12:50 +0800

    Upload ch14 book exercise 1

---
+++ 20170403_2Note/filefolder/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string path = Server.MapPath("Test");
+        int i = 0;
+        DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+        /* 取得目錄清單 */
+        try {
+            DirectoryInfo[] subDirs = dirInfo.GetDirectories();
+            for ( i = 0; i < subDirs.Length; i++ ) {
+                lblOutput.Text += subDirs[i].Name + "<br />";
+            }
+        } catch ( DirectoryNotFoundException ex ){
+            lblOutput.Text += ex.Message + "<br />";
+        }
+
+        /* 取得檔案清單 */
+        try {
+            FileInfo[] subFiles = dirInfo.GetFiles();
+            foreach ( FileInfo subFile in subFiles) {
+                lblOutput.Text += subFile.Name + "<br />";
+            }
+        } catch (DirectoryNotFoundException ex) {
+            lblOutput.Text += ex.Message + "<br />";
+        }
+
+    }
+}
