eb80c6f94751fa9cf7f0fb5b783242d457e5e814
kancheng <zxdfgcv@gmail.com>
2017-04-07 02:23:29 +0800

    Upload ch14 exercise practice 1 & 2

---
+++ 20170403_2Note/filefolderpra/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Dir :<br />
+        <br />
+    
+        <asp:Label ID="lblOutput" runat="server"></asp:Label>
+    
+        <br />
+        <br />
+        Write :<br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label">
+            鶴鳴于九皋，聲聞于野。魚潛在淵，或在于渚。樂彼之園，爰有樹檀，其下維萚。他山之石，可以為錯。
+            鶴鳴于九皋，聲聞于天。魚在于渚，或潛在淵。樂彼之園，爰有樹檀，其下維榖。他山之石，可以攻玉。
+        </asp:Label>
+    
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Submit" Width="15%" OnClick="Button1_Click" />
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server"></asp:Label>
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170403_2Note/filefolderpra/Default.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string path = Server.MapPath("Test");
+        int i = 0;
+        DirectoryInfo dirInfo = new DirectoryInfo(path);
+        FileInfo fInfo = new FileInfo(path);
+
+        /* 取得目錄清單 */
+        try
+        {
+            DirectoryInfo[] subDirs = dirInfo.GetDirectories();
+            for (i = 0; i < subDirs.Length; i++)
+            {
+                lblOutput.Text += subDirs[i].Name + "<br />";
+            }
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            lblOutput.Text += ex.Message + "<br />";
+        }
+
+        /* 取得檔案清單 */
+        try
+        {
+            FileInfo[] subFiles = dirInfo.GetFiles();
+
+            foreach (FileInfo subFile in subFiles)
+            {
+                lblOutput.Text += subFile.Name + "<br />";
+            }
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string path = Server.MapPath("Test");
+        string path2 = Server.MapPath("Test\\book.txt");
+        DirectoryInfo dirInfo = new DirectoryInfo(path);
+        FileInfo[] subFiles = dirInfo.GetFiles();
+
+        foreach (FileInfo subFile in subFiles)
+        {
+            if (!(subFile.Name == "book.txt")) {
+
+                FileInfo finfo = new FileInfo(path2);
+
+                StreamWriter sw = finfo.CreateText();
+                    // 欲寫入的文字資料
+                    sw.Write(Label1.Text + "\r\n");
+                sw.Flush();
+                sw.Close();
+                Label2.Text = "已寫入";
+            }
+        }
+
+    }
+}
