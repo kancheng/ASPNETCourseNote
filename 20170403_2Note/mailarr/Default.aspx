70337fa1e038dd3c779c742c48e88480580da5fb
kancheng <zxdfgcv@gmail.com>
2017-04-07 03:52:09 +0800

    Upload ch14 exercsie practice 4

---
+++ 20170403_2Note/mailarr/Default.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+
+        .style1
+        {
+            width: 100%;
+        }
+        .style5
+        {
+            color: #0000FF;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="style1">
+                <tr>
+                    <td class="style5">郵件地址:</td>
+                    <td>
+                        <asp:TextBox ID="txtMailTo" runat="server" Width="283px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style5">郵件主旨:</td>
+                    <td>
+                        <asp:TextBox ID="txtSubject" runat="server" Width="282px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style4" valign="top"><span class="style5">郵件內容:</span><asp:CheckBox ID="chkFormat" runat="server" ForeColor="Blue" Text="HTML" />
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtBody" runat="server" Height="101px" TextMode="MultiLine" Width="287px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style4" colspan="2" valign="top">
+                        <asp:FileUpload ID="fupFile" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="style3" colspan="2">&nbsp;
+                        <asp:Button ID="Button1" runat="server" Text="寄出" Width="109px" OnClick="Button1_Click" />
+                    </td>
+                </tr>
+            </table>
+            <asp:Label ID="lblOutput" runat="server" ForeColor="Red"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 20170403_2Note/mailarr/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string[] emt = new string[5];
+        emt[0] = "xxxx1@gmail.com";
+        emt[1] = "xxxx2@gmail.com";
+        // 建立MailMessage物件
+        MailMessage myMail = new MailMessage();
+        // 寄件者和收件者郵件地址
+        myMail.From = new MailAddress("jojo@gm.com");
+        myMail.To.Add(new MailAddress(emt[0]));
+        myMail.CC.Add(new MailAddress(emt[1]));
+        myMail.Priority = MailPriority.Normal; // 優先等級
+        myMail.Subject = txtSubject.Text;      // 主旨 
+        if (chkFormat.Checked) {               // HTML格式 
+            myMail.IsBodyHtml = true;
+            myMail.Body = "<p>" + txtBody.Text + "</p>";
+        }
+        else {                               // 文字格式
+            myMail.Body = txtBody.Text;      // 本文
+        }
+        // 處理郵件的附檔
+        string uploadFile = "~/temp/";
+        if (fupFile.HasFile) {   // 是否有上傳檔案
+            uploadFile += fupFile.FileName; // 建立路徑
+            // 取得實際的路徑
+            uploadFile = Server.MapPath(uploadFile);
+            fupFile.SaveAs(uploadFile);   // 儲存上傳檔案
+            // 在電子郵件加上附檔
+            Attachment attachedFile = new Attachment(uploadFile);
+            attachedFile.Name = fupFile.FileName;
+            myMail.Attachments.Add(attachedFile);  // 新增附檔
+        }
+        // 設定SMTP伺服器
+        SmtpClient smtpServer = new SmtpClient();
+        smtpServer.Host = "ms2.hinet.net";
+        CredentialCache myCache = new CredentialCache();
+        myCache.Add("ms2.hinet.net", 25, "login",
+                new NetworkCredential("zxdfgcv", "9487"));
+        smtpServer.Credentials = myCache;
+        smtpServer.Send(myMail);          // 寄出郵件
+        lblOutput.Text = "附檔電子郵件已經寄出.....<br/>";
+    }
+}
