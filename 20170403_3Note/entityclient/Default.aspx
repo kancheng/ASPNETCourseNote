68b7170ea595bf3fabb0c4c15b9a4d2b80d3c7df
kancheng <zxdfgcv@gmail.com>
2017-04-08 08:08:32 +0800

    Upload ch15 book exercise 6

---
+++ 20170403_3Note/entityclient/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+
+        .style1
+        {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="style1">
+                <tr>
+                    <td align="center">
+                        <asp:Button ID="Button1" runat="server" Text="取得學生姓名" OnClick="Button1_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center">
+                        <asp:TextBox ID="txtOutput" runat="server" Height="142px" ReadOnly="True" TextMode="MultiLine" Width="281px"></asp:TextBox>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 20170403_3Note/entityclient/Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.EntityClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        EntityConnection objCon;
+        EntityCommand objCmd;
+        EntityDataReader objDataReader;
+        string strSQL;
+
+        /* 建立 Connection 物件 */
+        objCon = new EntityConnection("name = SchoolEntities");
+
+        /* 開啟連結 */
+        objCon.Open();
+        strSQL = "SELECT [S].[name] FROM ";
+        strSQL += "[SchoolEntities].[Students] As S";
+
+        /* 建立 Command 物件 */
+        objCmd = objCon.CreateCommand();
+
+        /* 指定 SQL 指令 */
+        objCmd.CommandText = strSQL;
+
+        /* 執行 SQL 指令 */
+        objDataReader = objCmd.ExecuteReader(System.Data.CommandBehavior.SequentialAccess);
+        txtOutput.Text = "";
+
+        /* 讀取記錄資料 */
+        while (objDataReader.Read()) {
+            txtOutput.Text += objDataReader["name"] + "\r\n" ;
+        }
+
+        /* 關閉 DataReader 物件 */
+        objDataReader.Close();
+
+        /* 關閉連結 */
+        objCon.Close();
+    }
+}
