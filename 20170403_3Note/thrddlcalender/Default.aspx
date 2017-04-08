987ec3ef998bb4e3c1747eb38642f441f4d6e0cc
kancheng <zxdfgcv@gmail.com>
2017-04-08 10:43:13 +0800

    Upload ch15 exercise practice 1

---
+++ 20170403_3Note/thrddlcalender/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" >
+    <div style = "text-align:center;">
+        <br />
+        <div><h1>Day</h1></div>
+        <br />
+        <div>
+            <span style="margin-left:2em"></span>
+            <span> 年 : </span>
+            <span style="margin-left:3em"></span>
+            <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True"></asp:DropDownList>
+            <span style="margin-left:2em"></span>
+            <span> 月 : </span>
+            <span style="margin-left:3em"></span>
+            <asp:DropDownList ID="DropDownList2" runat="server" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged" AutoPostBack="True"></asp:DropDownList>
+        </div>
+        <br />
+        <div>
+            <asp:Calendar ID="Calendar1" runat="server" Width="100%" OnSelectionChanged="Calendar1_SelectionChanged" ></asp:Calendar>
+        </div>
+        <br />
+        <div>
+            <h2>日期</h2>
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 20170403_3Note/thrddlcalender/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    static int y, temy, m, temm, d;
+    static _Default (){
+
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack) // First time actions
+        {
+
+            for (y = 0; y < 100; y++)
+            {
+                DropDownList1.Items.Add((DateTime.Now.Year - y).ToString());
+            }
+
+            for (m = 1; m < 13; m++)
+            {
+                DropDownList2.Items.Add((m).ToString());
+            }
+            DropDownList1.SelectedValue = Convert.ToString(DateTime.Now.Year);
+            DropDownList2.SelectedValue = Convert.ToString(DateTime.Now.Year);
+        }
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        /*1. Calendar.TodaysDate */
+        Calendar1.TodaysDate = new DateTime(Convert.ToInt32(DropDownList1.SelectedValue), Convert.ToInt32(DropDownList2.SelectedValue), 1);
+        /* 2. Calendar.VisibleDate */
+        Calendar1.VisibleDate = new DateTime(Convert.ToInt32(DropDownList1.SelectedValue), Convert.ToInt32(DropDownList2.SelectedValue), 1);
+    }
+
+    protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+           Calendar1.TodaysDate = new DateTime(Convert.ToInt32(DropDownList1.SelectedValue), Convert.ToInt32(DropDownList2.SelectedValue), 1);
+           Calendar1.VisibleDate = new DateTime(Convert.ToInt32(DropDownList1.SelectedValue), Convert.ToInt32(DropDownList2.SelectedValue), 1);
+       }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        Label1.Text = Calendar1.SelectedDate.ToShortDateString();
+    }
+}
+ 
